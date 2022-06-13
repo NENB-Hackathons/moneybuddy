ecf1a844ae57a69c82a1114e13f8cac292f6d8e1
nobody5050 <levibelland@gmail.com>
2022-06-12 21:25:01 -0500

    dashboard changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,14 @@
 <template>
-<div
+  <div
+    class="
+      flex flex-grow
+      justify-center
+      items-start
+      bg-gray-100
+      dark:bg-gray-800
+    "
+  >
+    <div
       class="
         text-center
         w-full
@@ -13,36 +22,38 @@
     >
       <h2
         class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl"
-      >
-      </h2>
+      ></h2>
       <div class="lg:mt-0 lg:flex-shrink-0">
         <div class="mt-12 inline-flex rounded-md shadow">
-            <button
-              type="button"
-              class="
-                py-4
-                px-6
-                bg-indigo-600
-                hover:bg-indigo-700
-                focus:ring-indigo-500 focus:ring-offset-indigo-200
-                text-white
-                w-full
-                transition
-                ease-in
-                duration-200
-                text-center text-base
-                font-semibold
-                shadow-md
-                focus:outline-none focus:ring-2 focus:ring-offset-2
-                rounded-lg
-              "
-            >
-              Take QUiz
-            </button>
+          <button
+            type="button"
+            class="
+              py-4
+              px-6
+              bg-indigo-600
+              hover:bg-indigo-700
+              focus:ring-indigo-500 focus:ring-offset-indigo-200
+              text-white
+              w-full
+              transition
+              ease-in
+              duration-200
+              text-center text-base
+              font-semibold
+              shadow-md
+              focus:outline-none focus:ring-2 focus:ring-offset-2
+              rounded-lg
+            "
+          >
+            Take QUiz
+          </button>
         </div>
       </div>
-</div>
-  <DoughnutChart />
+    </div>
+    <div class="w-1/2">
+      <DoughnutChart :width="20" :height="20" />
+    </div>
+  </div>
 </template>
 
 <script>
