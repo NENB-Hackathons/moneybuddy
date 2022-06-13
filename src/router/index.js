a336cf547325c81d2dbeeae72682f629a35b56b1
nobody5050 <levibelland@gmail.com>
2022-06-12 19:54:29 -0500

    fix build issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
+import store from '@/store'
+let loggedin = store.state.isLoggedIn
+
 const routes = [{
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Register.vue"),
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Login.vue"),
-    },
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        beforeEnter: function(to, from, next) {
-            if (!loggedin) {
-                next('/login')
-            }
-        },
-        meta: {
-            isLoggedIn: true
-        },
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/Dashboard.vue"),
+  path: "/",
+  name: "Home",
+  component: Home,
+},
+{
+  path: "/register",
+  name: "Register",
+  component: () =>
+    import( /* webpackChunkName: "about" */ "../views/Register.vue"),
+},
+{
+  path: "/login",
+  name: "Login",
+  component: () =>
+    import( /* webpackChunkName: "about" */ "../views/Login.vue"),
+},
+{
+  path: "/dashboard",
+  name: "Dashboard",
+  beforeEnter: function (to, from, next) {
+    if (!loggedin) {
+      next('/login')
     }
+  },
+  meta: {
+    isLoggedIn: true
+  },
+  component: () =>
+    import( /* webpackChunkName: "about" */ "../views/Dashboard.vue"),
+}
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
